e434e6e63ee1d09b5809998444be337047664f2b
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-10-12 14:54:38 +0530

    Add templates to closest pair module.

--- Geometry/ClosestPair.hh
+++ Geometry/ClosestPair.hh
@@ -1,17 +1,18 @@
 #pragma once
 #include "Point.hh"
 
+template<typename T>
 void dnc(size_t lx, size_t rx,
-         Ptype &opt, Point &_p, Point &_q, vector<Point> &v) {
+         T &opt, Point<T> &_p, Point<T> &_q, vector<Point<T>> &v) {
   if( rx - lx == 1 ) return;
   auto mid = (lx + rx)>>1;
-  Ptype xmid = v[mid].x;
+  T xmid = v[mid].x;
   dnc(lx, mid, opt, _p, _q, v);
   dnc(mid, rx, opt, _p, _q, v);
   inplace_merge(v.begin()+lx, v.begin()+mid, v.begin()+rx,
-                [&](const Point&p, const Point&q) {
+                [&](const Point<T>&p, const Point<T>&q) {
                   return p.y!=q.y?p.y<q.y:p.x<q.x;} );
-  vector< Point > a;
+  vector< Point<T> > a;
   for(size_t i=lx; i<rx; i++)
     if( (v[i].x-xmid) * (v[i].x-xmid) <= opt )
       a.emplace_back(v[i]); // 2 * opt strip
@@ -22,12 +23,13 @@ void dnc(size_t lx, size_t rx,
   opt = temp;
 }
 
-pair<Point, Point> closest_pair(vector<Point> &v) {
-  sort(v.begin(), v.end(), [&](const Point&p, const Point&q)
+template<typename T>
+pair<Point<T>, Point<T>> closest_pair(vector<Point<T>> &v) {
+  sort(v.begin(), v.end(), [&](const Point<T>&p, const Point<T>&q)
                            {return p.x!=q.x?p.x<q.x:p.y<q.y;});
   assert(v.size() > 1);
-  Point ans[2]; ans[0] = v[0], ans[1] = v[1];
-  Ptype opt = (ans[1]-ans[0]).norm2();
+  Point<T> ans[2]; ans[0] = v[0], ans[1] = v[1];
+  T opt = (ans[1]-ans[0]).norm2();
   dnc(0, v.size(), opt, ans[0], ans[1], v);
   return make_pair(ans[0], ans[1]);
 }
