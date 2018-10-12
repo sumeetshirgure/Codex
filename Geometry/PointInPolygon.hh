28dc20fce62548f098e8326288b392f4ee27211e
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-10-12 21:19:15 +0530

    Add O(lg(n)) point in convex polygon.

--- Geometry/PointInPolygon.hh
+++ Geometry/PointInPolygon.hh
@@ -25,3 +25,28 @@ int point_in_polygon(const Point<T>&p, const vector<Point<T>>&v) {
   return 1; // Report -ve winding numbers separately.
 } // 0 : on, +1 : out, -1 : in
 
+/* Assumes polygon is given in standard form (as returned by convex_hull). */
+template<typename T>
+int point_in_convex_polygon(const Point<T>&p, const vector<Point<T>>&v) {
+  if( p == v[0] ) return 0;
+  int n = v.size(); auto pv = p - v[0];
+  if( ((v[1]-v[0])*pv) < 0 ) return 1;
+  if( ((v[n-1]-v[0])*pv) > 0 ) return 1;
+  int lo = 1, hi = n-1, mid;
+  while(lo < hi) {
+    mid = (lo+hi+1)/2;
+    if( ((v[mid]-v[0])*pv) >= 0 ) lo = mid;
+    else hi = mid-1;
+  }
+  int i = lo, j = i+1;
+  if( ( (p - v[i])*pv ) == 0 ) {
+    auto dot = ( (p - v[i]) ^ pv );
+    if( dot < 0 ) return i == 1 or j == n ? 0 : -1;
+    if( dot == 0 ) return 0;
+    return 1;
+  }
+  auto cross = ( (v[j] - v[i]) * (p - v[i]) );
+  if( cross > 0 ) return -1;
+  if( cross == 0 ) return 0;
+  return 1;
+}
