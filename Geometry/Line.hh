485a32f74ef7de7d709dc922144e3f731ac6c72f
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-10-12 12:34:23 +0530

    Add templates to line class.

--- Geometry/Line.hh
+++ Geometry/Line.hh
@@ -1,47 +1,37 @@
 #pragma once
 #include "Point.hh"
 
+#define Ptype ll
+
+template<typename T>
 struct Line {
-  Ptype a, b, c;
-  Line(const Point&p, const Point&q) {
+  T a, b, c;
+  Line(const Point<T>&p, const Point<T>&q) {
     a = q.y-p.y, b = p.x-q.x;
     // auto g = __gcd(a, b);
     // a /= g, b /= g;
     c = -(a*p.x+b*p.y);
   }
-  Line (const Ptype&aa=0, const Ptype&bb=0, const Ptype&cc=0) :
+  Line (const T&aa=0, const T&bb=0, const T&cc=0) :
     a(aa), b(bb), c(cc) { }
-  inline Ptype fxval(const Point&p) const &
-  { return a*p.x + b*p.y + c; }
-  inline Ptype dist2(const Point&p) const& {
-    Ptype fx = fxval(p);
-    return (fx * fx) / (a*a+b*b);
-  }
-  Ftype distance(const Point &p) const &
-  { return sqrt(dist2(p)); }
+  inline T fxval(const Point<T>&p) const { return a*p.x + b*p.y + c; }
+  Point<T> dir() const { return Point<T>(a, b); }
 };
-ostream& operator<<(ostream &os, const Line &l)
+
+template<typename T> ostream& operator<<(ostream &os, const Line<T>&l)
 { return os<<'['<<l.a<<" * x + "<<l.b<<" * y + "<<l.c<<" =0]"; }
 
-/* Projection formula : k=-1 for foot of perpendicular,
-   -2 for reflection about line. */
-Point projection(const Point&p, const Line&line, const Ftype &k=-1.) {
-  auto temp = k*line.fxval(p)/(line.a*line.a+line.b*line.b);
-  return Point(p.x + line.a*temp, p.y + line.b*temp);
-} // Extend to 3D
+template<typename U, typename V>
+long double dist2(const Line<U>&line, const Point<V>&p)
+{ long double fx = line.fxval(p); return (fx * fx) / line.dir().norm2(); }
+template<typename U, typename V>
+long double dist(const Line<U>&line, const Point<V>&p)
+{ long double fx = line.fxval(p); return abs(fx) / line.dir().norm(); }
+// { return sqrt( dist2(line, p) ); }
 
-/* Line-Line cut points. Returns <number_of_points, point_if_unique> */
-tuple< int , Point > cut(const Line &A, const Line &B) {
-  int cutPoints = 0; Point ret;
-  Ptype det = A.a*B.b - A.b*B.a;
-  if( abs( det ) <= eps ) {
-    cutPoints = 0; // Degenerate case.
-    if( abs( A.a*B.c - A.c*B.a ) <= eps )
-      cutPoints = -1; // Infinitely many.
-  } else {
-    cutPoints = 1; // Unique point of intersection.
-    ret.x = ( A.b*B.c - A.c*B.b ) / det ;
-    ret.y = ( A.c*B.a - A.a*B.c ) / det ;
-  }
-  return tie(cutPoints , ret);
-}
+/* Projection formula : k=-1 for foot, -2 for reflection. */
+template<typename U, typename V, typename W> Point<U> projection
+(const Point<U>&p, const Line<V>&line, const W&k=-1) {
+  U temp = line.fxval(p)/((U) line.dir().norm2());
+  return Point(p.x + line.a * temp * k, p.y + line.b * temp * k);
+} // Extend to 3D.
