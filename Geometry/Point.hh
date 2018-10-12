3a9963cbe99275a9ba04f2a8434ac4cb40beee32
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-10-12 11:34:27 +0530

    Add templates to point class.

--- Geometry/Point.hh
+++ Geometry/Point.hh
@@ -1,47 +1,57 @@
 #pragma once
 #include "../Header.hh"
 
-using Ptype = long long;   // discrete metrics
-Ptype eps = (Ptype)(1e-6); // fine tune
-using Ftype = long double; // all other metrics
-
+template<typename T>
 struct Point {
-  Ptype x, y;
-  inline Ptype norm2() const& {return x*x+y*y;}
-  Ftype norm() const& {return sqrt(norm2()); }
-  Ftype arg() const& {return atan2(y, x);}
+  T x, y;
+  Point() : x(0), y(0) {}
+  Point(const T&_x, const T&_y) : x(_x), y(_y) {}
+  inline T norm2() const& { return x*x+y*y; }
+  inline auto norm() -> decltype(sqrt(norm2())) {return sqrt(norm2());}
+  inline auto arg()  -> decltype(atan2(y, x))   {return atan2(y, x);}
 };
-Point operator+(const Point&p, const Point&q)
-{ return Point({p.x+q.x, p.y+q.y}); }
-Point operator-(const Point&p, const Point&q)
-{ return Point({p.x-q.x, p.y-q.y}); }
-Point operator*(const Point &p, const Ptype &k)
-{ return Point({k*p.x,k*p.y}); }
-Point operator*(const Ptype &k, const Point &p)
-{ return Point({k*p.x,k*p.y}); }
-Ptype operator*(const Point &p, const Point &q)
+
+/* Sums. */
+template<typename T> Point<T> operator+(const Point<T>&p, const Point<T>&q)
+{ return Point<T>(p.x+q.x, p.y+q.y); }
+template<typename T> Point<T> operator-(const Point<T>&p, const Point<T>&q)
+{ return Point<T>(p.x-q.x, p.y-q.y); }
+
+/* Scalar products. */
+template<typename T, typename F> Point<T> operator*(const Point<T>&p, const F&k)
+{ return Point<T>(k*p.x,k*p.y); }
+template<typename T, typename F> Point<T> operator*(const F&k, const Point<T>&p)
+{ return Point<T>(k*p.x,k*p.y); }
+template<typename T, typename F> Point<T> operator/(const Point<T>&p, const F&k)
+{ return Point(p.x/k,p.y/k); }
+
+/* Vector products. */
+template<typename T> T operator*(const Point<T>&p, const Point<T>&q)
 { return p.x*q.y - q.x*p.y; }
-Point operator/(const Point &p, const Ptype &k)
-{ return Point({p.x/k,p.y/k}); }
-Ptype operator^(const Point &p, const Point &q)
+template<typename T> T operator^(const Point<T>&p, const Point<T>&q)
 { return p.x*q.x + p.y*q.y; }
-bool operator==(const Point&p, const Point&q)
-{ return abs(p.x-q.x) <= eps and abs(p.y-q.y) <= eps; }
-ostream& operator<<(ostream &out, const Point&p)
+
+/* Comparators. */
+template<typename T> inline bool operator< (const Point<T>&p, const Point<T>&q)
+{ return p.x != q.x ? p.x < q.x : p.y < q.y; }
+template<typename T> inline bool operator==(const Point<T>&p, const Point<T>&q)
+{ return (p.x == q.x) and (p.y == q.y); } // Don't use == for floats.
+
+/* Debugger. */
+template<typename T> ostream& operator<<(ostream &out, const Point<T>&p)
 { return out << '[' << p.x << ' ' << p.y << ']'; }
 
 /* Returns twice the signed area. */
-Ptype signedAreax2(const vector<Point> &v) {
-  Ptype ret = 0;
+template <typename T> T signedAreax2(const vector< Point<T> > &v) {
+  T ret = 0;
   for(size_t i=0, j=v.size()-1; i<v.size(); j=i++)
     ret += (v[i].x-v[j].x)*(v[i].y+v[j].y);
   return ret;
 }
 
-/* First translate to new origin, then rotate by theta. */
-Point affineTransform (const Point &point,
-                       const Point &pivot, const Ftype &theta) {
-  Point t = point - pivot;
-  Ftype C = cos(theta), S = sin(theta);
-  return Point({t.x*C-t.y*S, t.x*S+t.y*C}) ;
-} // PType must be float
+/* Rotate by theta around pivot. */
+template<typename T, typename F> Point<T> affineTransform
+(const Point<T>&point, const Point<T> &pivot, const F &theta) {
+  auto t = point - pivot; auto C = cos(theta), S = sin(theta);
+  return pivot + Point<T>(t.x*C-t.y*S, t.x*S+t.y*C) ;
+} // T must be float.
