d1a51e94d681d53b066642bcaa0d2a2f4975c523
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-10-12 16:12:52 +0530

    Add circle.

---
+++ Geometry/Circle.hh
@@ -0,0 +1,18 @@
+#pragma once
+#include "Point.hh"
+#include "Cuts.hh"
+
+template<typename T> Line<T> halfline(const Point<T> &p, const Point<T> &q) {
+  auto mpt = (p+q)/2, dir = q-p;
+  Line<T> line(dir.x, dir.y, 0); line.c = -line.fxval(mpt);
+  return line;
+}
+
+template<typename T> tuple<T, Point<T>> make_circle
+(const Point<T> &r, const Point<T> &s, const Point<T> &t) {
+  if( ( (s-r)*(t-r) ) <= EPS ) return make_tuple(-1, Point<T>());
+  auto l1 = halfline(r, s), l2 = halfline(s, t);
+  Point<T> center; tie(ignore, center) = cut(l1, l2);
+  T radius = (center-s).norm();
+  return tie(radius, center);
+}
