849c0ddf5b6f185579db747b0dd0b450386b7b33
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-11-07 11:49:29 +0530

    Add binary search.

---
+++ Range/BinarySearch.hh
@@ -0,0 +1,17 @@
+#pragma once
+
+template<typename T, typename Predicate>
+T first_value(const T &lower, const T &upper, const Predicate &predicate) {
+  T lo = lower, up = upper, mid; // predicate(upper) is assumed.
+  while( lo < up ) { mid = (lo + hi) / 2;
+    if( predicate(mid) ) up = mid; else lo = mid+1; }
+  return lo;
+}
+
+template<typename T, typename Predicate>
+T last_value (const T &lower, const T &upper, const Predicate &predicate) {
+  T lo = lower, up = upper, mid; // predicate(lower) is assumed.
+  while( lo < up ) { mid = (lo + hi + 1) / 2;
+    if( predicate(mid) ) lo = mid; else up = mid-1; }
+  return lo;
+}
