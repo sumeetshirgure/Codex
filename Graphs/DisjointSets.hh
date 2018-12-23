c6b910119325858ae0caa1fcd6a6e45a02d45cf8
Sumeet Shirgure <sumeetshirgure@gmail.com>
2018-12-23 22:08:07 +0530

    Upgrade DSU. Revert back to rank compression.

--- Graphs/DisjointSets.hh
+++ Graphs/DisjointSets.hh
@@ -4,31 +4,29 @@
 /* Disjoint set union-find structure. Uses path compression. */
 template<typename T>
 struct DSU {
-  vector<int> par, size;
+  vector<int> par, rank;
   vector<T> data;
 
   DSU(const size_t &_n) {
-    par.resize(_n), size.resize(_n);
+    par.resize(_n), rank.resize(_n);
     data.resize(_n);
     iota(par.begin(), par.end(), 0);
-    fill(size.begin(), size.end(), 1);
+    fill(rank.begin(), rank.end(), 1);
   }
 
-  inline int find(int x) {
-    int y, t;
-    for(y = x; par[y] != y; y = par[y]);
-    for(; x != y; t = par[x], par[x] = y, x = t);
-    return y; // return par[x] == x ? x : par[x] = find(par[x]);
+  int find(int x) {
+    return par[x] == x ? x : par[x] = find(par[x]);
   }
 
-  bool unite(int u, int v) {
+  int unite(int u, int v) {
     u = find(u), v = find(v);
-    if( u == v ) return false;
-    if( size[v] > size[u] ) swap(u, v);
-    size[u] += size[v];
-    par[v] = u;
-    data[u] = combine(data[u], data[v]);
-    return true;
+    if( u == v ) return u;
+    int parent = u, child = v;
+    if( rank[child] > rank[parent] ) swap(parent, child);
+    if( rank[parent] == rank[child] ) rank[parent]++;
+    par[child] = parent;
+    data[parent] = combine(data[u], data[v]);
+    return parent;
   }
 
   T& operator[](const size_t &index) {
@@ -37,7 +35,10 @@ struct DSU {
     return data.at( find( index ) );
   }
 
-  inline void makeParent(int u) {
-    par[u] = par[find(u)] = u;
+  inline void make_parent(int u) {
+    int v = find(u);
+    if( u == v ) return;
+    par[u] = par[v] = u;
+    swap(data[u], data[v]);
   }
 };
